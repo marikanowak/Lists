d120db8232cb0eb2d8068c1f0c340045ad532276
marikanowak <95419013+marikanowak@users.noreply.github.com>
2021-12-02 14:02:50 +0100

    deleted unused prints

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,10 +10,6 @@ public class Main {
         System.out.println(cars.get(0));
         System.out.println(cars.get(1));
         System.out.println(cars.get(2));
-        System.out.println(cars.get(3));
-        System.out.println(cars.get(4));
-        System.out.println(cars.get(5));
-        System.out.println(cars.get(6));
-        System.out.println(cars.get(7));
+
     }
 }
